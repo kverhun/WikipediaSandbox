e9d42f5dc0e347599a39cbc3d6ea69bbadcfe37e
kverhun <k.verhun@gmail.com>
2016-11-20 18:06:56 +0200

    add qt dependency; show main window

--- sources/gui/main.cpp
+++ sources/gui/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+#include <QApplication>
+#include <QMainWindow>
+
 int main(int i_argc, char** i_argv)
 {
     std::cout << "Wikipedia Sandbox" << std::endl;
-    return 0;
+
+    QApplication app(i_argc, i_argv);
+    QMainWindow wnd;
+    wnd.show();
+
+    auto exit_code = app.exec();
+
+    return exit_code;
 }
