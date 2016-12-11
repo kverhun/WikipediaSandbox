dcd3e7c567f7066ccae766261ec8b100a203d161
kverhun <k.verhun@gmail.com>
2016-12-11 18:36:38 +0200

    Create cluster graph

--- sources/Libraries/GraphClusterization/Clusterization.cpp
+++ sources/Libraries/GraphClusterization/Clusterization.cpp
@@ -1,5 +1,7 @@
 #include "Clusterization.h"
 
+#include <set>
+
 using namespace GraphClusterization;
 using Graphs::Graph;
 
@@ -17,5 +19,22 @@ std::shared_ptr<Graph> Clusterization::GetClusterGraph() const
 
 std::shared_ptr<Graph> Clusterization::_CreateClusterGraph()
 {
-    return nullptr;
+    std::set<Graph::TVertex> vertices;
+    for (const auto& entry : m_cluster_map)
+        vertices.insert(entry.second);
+
+    std::map<TClusterId, Graph::TVertices> cluster_to_vertices;
+    for (const auto& entry : m_cluster_map)
+        cluster_to_vertices[entry.second].push_back(entry.first);
+
+    std::set<Graph::TEdge> edges;
+    for (const auto& e : m_graph.GetEdges())
+    {
+        auto cluster_from = m_cluster_map[e.first];
+        auto cluster_to = m_cluster_map[e.second];
+        if (cluster_from != cluster_to)
+            edges.insert(std::make_pair(cluster_from, cluster_to));
+    }
+
+    return std::make_shared<Graph>(Graph::TVertices(vertices.begin(), vertices.end()), Graph::TEdges(edges.begin(), edges.end()));
 }
--- sources/Libraries/GraphClusterization_UnitTests/GraphClusterization_UnitTests.cpp
+++ sources/Libraries/GraphClusterization_UnitTests/GraphClusterization_UnitTests.cpp
@@ -9,12 +9,50 @@ using Graphs::Graph;
 
 TEST_CASE("CreateClusterization")
 {
-	SECTION("DummyTest")
-	{
+    SECTION("TestClusterGraphCreation1")
+    {
         Graph g{ {1,2}, {{1, 2}} };
         TClusterMap cluster_map{ {1,1}, {2,2} };
 
         Clusterization clusterization{ cluster_map, g };
-        REQUIRE(nullptr == clusterization.GetClusterGraph());
+        auto p_cluster_graph = clusterization.GetClusterGraph();
+
+        const Graph::TVertices vertices_expected{ 1, 2 };
+        const Graph::TEdges edges_expected{ {1, 2} };
+
+        REQUIRE(nullptr != p_cluster_graph);
+        REQUIRE(vertices_expected == p_cluster_graph->GetVertices());
+        REQUIRE(edges_expected == p_cluster_graph->GetEdges());
 	}
+
+    SECTION("TestClusterGraphCreation2")
+    {
+        Graph g{ { 1,2,3,4 },{ { 1, 2 },{1,3},{3,2},{4,1},{4,3} } };
+        TClusterMap cluster_map{ { 1,1 },{ 2,1 },{3,2},{4,2} };
+
+        Clusterization clusterization{ cluster_map, g };
+        auto p_cluster_graph = clusterization.GetClusterGraph();
+
+        const Graph::TVertices vertices_expected{ 1, 2 };
+        const Graph::TEdges edges_expected{ { 1, 2 }, {2, 1}};
+
+        REQUIRE(nullptr != p_cluster_graph);
+        REQUIRE(vertices_expected == p_cluster_graph->GetVertices());
+        REQUIRE(edges_expected == p_cluster_graph->GetEdges());
+    }
+
+    SECTION("TestClusterGraphCreation3")
+    {
+        Graph g{ {1,2,3,4,5,6,7,8,9,10}, {{1,2}, {2,3}, {3,4}, {4,5}, {5,6}, {7,8}, {8,9}, {10,9}, {9,4}} };
+        TClusterMap cluster_map{ {1,1},{2,1}, {3,1}, {4,2}, {5,2},{6,2}, {7,3}, {8,3}, {9,3}, {10,3} };
+
+        Clusterization clusterization{ cluster_map, g };
+        auto p_cluster_graph = clusterization.GetClusterGraph();
+        const Graph::TVertices vertices_expected{1,2,3};
+        const Graph::TEdges edges_expected{ {1,2},{3,2} };
+
+        REQUIRE(nullptr != p_cluster_graph);
+        REQUIRE(vertices_expected == p_cluster_graph->GetVertices());
+        REQUIRE(edges_expected == p_cluster_graph->GetEdges());
+    }
 }
